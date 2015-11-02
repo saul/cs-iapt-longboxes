a3375191f31e2666f11a08d2d79effa2c8fe1032
Saul Rennison <saul.rennison@gmail.com>
2015-11-02 15:21:43 +0000

    Create 'Unfiled' box on registration

--- models/box.py
+++ models/box.py
@@ -2,7 +2,12 @@ import datetime
 
 db.define_table('box',
                 Field('owner', 'reference auth_user', required=True, notnull=True, writable=False),
-                Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY()),
+                Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY(), unique=True),
                 Field('private', 'boolean', required=True, notnull=True),
                 Field('created', 'datetime', required=True, notnull=True, default=datetime.datetime.now, writable=False)
                 )
+
+def on_user_created(form):
+    db.box.insert(owner=form.vars.id, name='Unfiled', private=True)
+
+auth.settings.register_onaccept.append(on_user_created)
