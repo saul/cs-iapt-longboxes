eba9c5a41529028405a67f070e5238f2f6f7956a
Saul Rennison <saul.rennison@gmail.com>
2015-11-02 15:23:08 +0000

    Added comicbox table after a fatal misunderstanding of the assessment rubric

--- models/comic.py
+++ models/comic.py
@@ -13,13 +13,16 @@ db.define_table('publisher',
                 )
 
 db.define_table('comic',
-                Field('box', db.box, required=True, notnull=True, requires=IS_IN_DB(db, 'box.id', '%(name)s', zero='Choose a box')),
                 Field('publisher', db.publisher, required=True, notnull=True, requires=IS_IN_DB(db, 'publisher.id', '%(name)s', zero='Choose publisher')),
                 Field('title', required=True, notnull=True, requires=IS_NOT_EMPTY()),
                 Field('issue', 'integer', required=True, notnull=True, requires=IS_INT_IN_RANGE(1, 1e100), default=1),
                 Field('description', 'text', required=True, notnull=True, requires=[IS_FEWER_WORDS(300), IS_NOT_EMPTY()]),
                 Field('cover_image', 'upload', required=True, notnull=True, requires=[IS_IMAGE()])
                 )
+
+db.define_table('comicbox',
+                Field('box', db.box, required=True, notnull=True),
+                Field('comic', db.comic, required=True, notnull=True))
 
 db.define_table('comicwriter',
                 Field('comic', db.comic, required=True, notnull=True),
