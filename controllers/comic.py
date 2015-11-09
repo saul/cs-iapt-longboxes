399eac12c492e18053ba20a2319fd14c0fe9c89c
Saul Rennison <saul.rennison@gmail.com>
2015-11-09 12:54:03 +0000

    Added /comic/create and /comic/edit functions

--- controllers/comic.py
+++ controllers/comic.py
@@ -1,32 +1,86 @@
-from helpers import flash_and_redirect_back
+from helpers import flash_and_redirect_back, get_or_create
+
+
+class ComicForm:
+    def __init__(self, record=None):
+        if record:
+            self.id = record.id
+
+            artists = record.comicartist(db.artist.id == db.comicartist.artist).select()
+            record.artists = ';'.join(map(lambda row: row.artist.name, artists))
+
+            writers = record.comicwriter(db.writer.id == db.comicwriter.writer).select()
+            record.writers = ';'.join(map(lambda row: row.writer.name, writers))
+
+            record.publisher = record.publisher.name
+        else:
+            self.id = None
+
+        fields = [field for field in db.comic if field.name != 'publisher']
+
+        # Only show a 'box' field if we're creating the comic
+        if not self.id:
+            user_boxes = [(row.id, row.name) for row in db(db.box.owner == auth.user.id).select(orderby=db.box.id)]
+            fields.insert(0, Field('box', 'reference box', requires=IS_IN_SET(user_boxes, zero=None)))
+
+        fields.insert(3, Field('publisher', 'string', requires=IS_NOT_EMPTY()))
+        fields.insert(4, Field('artists', 'string', requires=IS_NOT_EMPTY()))
+        fields.insert(5, Field('writers', 'string', requires=IS_NOT_EMPTY()))
+
+        self.form = SQLFORM.factory(
+            *fields,
+            record=record,
+            formstyle='table3cols',
+            table_name='comic'
+        )
+
+    def _on_success(self, form):
+        # Create the publisher if it doesn't exist already
+        publisher_id = get_or_create(db.publisher, name=form.vars.publisher)
+
+        comic_fields = db.comic._filter_fields(form.vars)
+        comic_fields['publisher'] = publisher_id
+
+        # Update the record if it's already in the database
+        if self.id:
+            db(db.comic.id == self.id).update(**comic_fields)
+        else:
+            self.id = db.comic.insert(**comic_fields)
+
+            # Add the comic to the box
+            db.comicbox.insert(comic=self.id, box=form.vars.box)
+
+        self._process_writers_and_artists()
+
+    def process(self, **kwargs):
+        kwargs['onsuccess'] = self._on_success
+        return self.form.process(**kwargs)
+
+    def _process_writers_and_artists(self):
+        db(db.comicartist.comic == self.id).delete()
+
+        for artist_name in self.form.vars.artists.split(';'):
+            artist_id = get_or_create(db.artist, name=artist_name)
+            db.comicartist.insert(comic=self.id, artist=artist_id)
+
+        db(db.comicwriter.comic == self.id).delete()
+
+        for writer_name in self.form.vars.writers.split(';'):
+            writer_id = get_or_create(db.writer, name=writer_name)
+            db.comicwriter.insert(comic=self.id, writer=writer_id)
 
 
 @auth.requires_login()
 def create():
-    fields = [field for field in db.comic]
-
-    # TODO: add writer and artist
-    # TODO: calculate which boxes this comic can be a part of
-    fields += [
-        Field('box', 'reference box', requires=IS_IN_SET(customer_types, zero=None, sort=False)),
-    ]
-
-    form = SQLFORM.factory(
-        *fields,
-        formstyle='table3cols',
-        table_name='comic'
-    )
+    form = ComicForm()
 
     if form.process().accepted:
-        comic_id = db.customer.insert(**db.comic._filter_fields(form.vars))
-
         session.flash = 'Created comic'
-
-        redirect(URL('comic', 'view', args=[comic_id]))
-    elif form.errors:
+        redirect(URL('comic', 'view', args=[form.id]))
+    elif form.form.errors:
         response.flash = 'Form has errors'
 
-    return {'form': form}
+    return {'form': form.form}
 
 
 @auth.requires_login()
@@ -44,14 +98,15 @@ def edit():
     if db(db.box.owner == auth.user.id)(db.comicbox.box == db.box.id)(db.comicbox.comic == comic.id).isempty():
         flash_and_redirect_back('You cannot edit a comic you did not create.')
 
-    form = SQLFORM(db.comic, comic, formstyle='table3cols')
+    form = ComicForm(comic)
 
     if form.process().accepted:
-        response.flash = 'Comic updated successfully'
-    elif form.errors:
+        session.flash = 'Comic updated successfully'
+        redirect(URL('comic', 'view', args=[form.id]))
+    elif form.form.errors:
         response.flash = 'Form has errors'
 
-    return {'form': form}
+    return {'form': form.form}
 
 
 def view():
