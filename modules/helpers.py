378b4ea75eb81b068e89c39eb6cdef2dbef64b55
Saul Rennison <saul.rennison@gmail.com>
2015-11-11 10:40:40 +0000

    Added get_or_404 helper

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,5 +1,5 @@
 from gluon.globals import current
-from gluon.http import redirect
+from gluon.http import redirect, HTTP
 from gluon.html import URL
 
 
@@ -10,6 +10,13 @@ def flash_and_redirect_back(flash):
         redirect(current.request.env.http_referer)
     else:
         redirect(URL('default', 'index'))
+
+
+def get_or_404(model, id, **kwargs):
+    record = model(id, **kwargs)
+    if not record:
+        raise HTTP(404)
+    return record
 
 
 def get_or_create(model, **fields):
