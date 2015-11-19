9ed08e77acc7a785e509d32ca103803dcd581e8a
Saul Rennison <saul.rennison@gmail.com>
2015-11-19 17:26:20 +0000

    Added ability to search only specific columns

--- controllers/comic.py
+++ controllers/comic.py
@@ -145,28 +145,38 @@ def view():
 
 
 def search():
-    search = request.get_vars.get('search', '')
+    original_search = request.get_vars.get('search', '')
 
     user_id = auth.user.id if auth.is_logged_in() else 0
 
     # generate a base query which finds all comics that the logged in user has permission to see
     base_query = db(db.comic.id == db.comicbox.comic)(db.comicbox.box == db.box.id)((db.box.private == False) | (db.box.owner == user_id))
 
-    def fuzzy_like(field):
-        return field.like('%{0}%'.format(search))
+    search_parts = original_search.split(':', 1)
 
-    queries = [
-        base_query(fuzzy_like(db.comic.title)),
-        base_query(db.publisher.id == db.comic.publisher)(fuzzy_like(db.publisher.name)),
-        base_query(db.comicwriter.comic == db.comic.id)(db.writer.id == db.comicwriter.writer)(fuzzy_like(db.writer.name)),
-        base_query(db.comicartist.comic == db.comic.id)(db.artist.id == db.comicartist.artist)(fuzzy_like(db.artist.name)),
-    ]
+    if len(search_parts) > 1:
+        only_field, search = search_parts
+    else:
+        only_field, search = None, original_search
 
-    query_results = map(lambda q: q.select(db.comic.ALL), queries)
+    fuzzy_like = lambda f: f.like('%{0}%'.format(search))
 
-    comics = reduce(lambda c, q: c | q, query_results)
+    queries = {
+        'title': base_query(fuzzy_like(db.comic.title)),
+        'publisher': base_query(db.publisher.id == db.comic.publisher)(fuzzy_like(db.publisher.name)),
+        'writer': base_query(db.comicwriter.comic == db.comic.id)(db.writer.id == db.comicwriter.writer)(fuzzy_like(db.writer.name)),
+        'artist': base_query(db.comicartist.comic == db.comic.id)(db.artist.id == db.comicartist.artist)(fuzzy_like(db.artist.name)),
+    }
+
+    if only_field and only_field in queries:
+        comics = queries[only_field].select(db.comic.ALL)
+    else:
+        query_results = map(lambda q: q.select(db.comic.ALL), queries.values())
+        comics = reduce(lambda c, q: c | q, query_results)
 
     return {
+        'original_search': original_search,
         'search': search,
-        'comics': comics
+        'comics': comics,
+        'only_field': only_field
     }
--- models/comic.py
+++ models/comic.py
@@ -2,34 +2,76 @@ import os
 
 from customvalidators import IS_FEWER_WORDS
 
+# ------------------------------------------------------------------------
+#  Writer
+# ------------------------------------------------------------------------
 db.define_table('writer',
                 Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY())
                 )
 
+
+class WriterVirtualFields:
+    def search_url(self):
+        return URL('comic', 'search', vars={'search': 'writer:' + self.writer.name})
+
+
+db.writer.virtualfields.append(WriterVirtualFields())
+
+# ------------------------------------------------------------------------
+#  Artist
+# ------------------------------------------------------------------------
 db.define_table('artist',
                 Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY())
                 )
+
+
+class ArtistVirtualFields:
+    def search_url(self):
+        return URL('comic', 'search', vars={'search': 'artist:' + self.artist.name})
+
+
+db.artist.virtualfields.append(ArtistVirtualFields())
+
+# ------------------------------------------------------------------------
+#  Publisher
+# ------------------------------------------------------------------------
 
 db.define_table('publisher',
                 Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY())
                 )
 
+
+class PublisherVirtualFields:
+    def search_url(self):
+        return URL('comic', 'search', vars={'search': 'publisher:' + self.publisher.name})
+
+
+db.publisher.virtualfields.append(PublisherVirtualFields())
+
+# ------------------------------------------------------------------------
+#  Comic
+# ------------------------------------------------------------------------
 db.define_table('comic',
-                Field('publisher', db.publisher, required=True, notnull=True, requires=IS_IN_DB(db, 'publisher.id', '%(name)s', zero='Choose publisher')),
+                Field('publisher', db.publisher, required=True, notnull=True,
+                      requires=IS_IN_DB(db, 'publisher.id', '%(name)s', zero='Choose publisher')),
                 Field('title', required=True, notnull=True, requires=IS_NOT_EMPTY()),
                 Field('issue', 'integer', required=True, notnull=True, requires=IS_INT_IN_RANGE(1, 1e100), default=1),
-                Field('description', 'text', required=True, notnull=True, requires=[IS_FEWER_WORDS(300), IS_NOT_EMPTY()]),
-                Field('cover_image', 'upload', required=True, notnull=True, requires=[IS_IMAGE(maxsize=(300, 400))], uploadfolder=os.path.join(os.path.dirname(__file__), '..', 'uploads'))
+                Field('description', 'text', required=True, notnull=True,
+                      requires=[IS_FEWER_WORDS(300), IS_NOT_EMPTY()]),
+                Field('cover_image', 'upload', required=True, notnull=True, requires=[IS_IMAGE(maxsize=(300, 400))],
+                      uploadfolder=os.path.join(os.path.dirname(__file__), '..', 'uploads'))
                 )
 
 
 class ComicVirtualFields:
     def owner(self):
-        return db(db.comicbox.comic == self.comic.id)(db.box.id == db.comicbox.box)(db.auth_user.id == db.box.owner).select(db.auth_user.ALL).first()
+        return db(db.comicbox.comic == self.comic.id)(db.box.id == db.comicbox.box)(
+            db.auth_user.id == db.box.owner).select(db.auth_user.ALL).first()
 
     def boxes(self):
         user_id = auth.user.id if auth.user else 0
-        return db(db.comicbox.comic == self.comic.id)(db.box.id == db.comicbox.box)((db.box.private == False) | (db.box.owner == user_id)).select(db.box.ALL)
+        return db(db.comicbox.comic == self.comic.id)(db.box.id == db.comicbox.box)(
+            (db.box.private == False) | (db.box.owner == user_id)).select(db.box.ALL)
 
     def full_name(self):
         return '%s #%s' % (self.comic.title, self.comic.issue)
@@ -40,16 +82,24 @@ class ComicVirtualFields:
 
 db.comic.virtualfields.append(ComicVirtualFields())
 
-
+# ------------------------------------------------------------------------
+#  Comic Box
+# ------------------------------------------------------------------------
 db.define_table('comicbox',
                 Field('box', db.box, required=True, notnull=True),
                 Field('comic', db.comic, required=True, notnull=True))
 
+# ------------------------------------------------------------------------
+#  Comic Writer
+# ------------------------------------------------------------------------
 db.define_table('comicwriter',
                 Field('comic', db.comic, required=True, notnull=True),
                 Field('writer', db.writer, required=True, notnull=True)
                 )
 
+# ------------------------------------------------------------------------
+#  Comic Artist
+# ------------------------------------------------------------------------
 db.define_table('comicartist',
                 Field('comic', db.comic, required=True, notnull=True),
                 Field('artist', db.artist, required=True, notnull=True)
