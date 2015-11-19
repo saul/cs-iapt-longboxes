ffe2572a61bb36a7f24cd73d26e854ace81819ad
Saul Rennison <saul.rennison@gmail.com>
2015-11-19 16:16:26 +0000

    Fixed comic image uploads, refer to comics by their name and issue number in front-end

--- models/comic.py
+++ models/comic.py
@@ -1,3 +1,5 @@
+import os
+
 from customvalidators import IS_FEWER_WORDS
 
 db.define_table('writer',
@@ -17,7 +19,7 @@ db.define_table('comic',
                 Field('title', required=True, notnull=True, requires=IS_NOT_EMPTY()),
                 Field('issue', 'integer', required=True, notnull=True, requires=IS_INT_IN_RANGE(1, 1e100), default=1),
                 Field('description', 'text', required=True, notnull=True, requires=[IS_FEWER_WORDS(300), IS_NOT_EMPTY()]),
-                Field('cover_image', 'upload', required=True, notnull=True, requires=[IS_IMAGE(maxsize=(400, 300))])
+                Field('cover_image', 'upload', required=True, notnull=True, requires=[IS_IMAGE(maxsize=(300, 400))], uploadfolder=os.path.join(request.folder, 'uploads'))
                 )
 
 
@@ -28,6 +30,9 @@ class ComicVirtualFields:
     def boxes(self):
         user_id = auth.user.id if auth.user else 0
         return db(db.comicbox.comic == self.comic.id)(db.box.id == db.comicbox.box)((db.box.private == False) | (db.box.owner == user_id)).select(db.box.ALL)
+
+    def full_name(self):
+        return '%s #%s' % (self.comic.title, self.comic.issue)
 
     def url(self):
         return URL('comic', 'view', args=[self.comic.id])
