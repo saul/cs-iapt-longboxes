73c5be015cb06e06143a940d8a2b53803cb826af
Saul Rennison <saul.rennison@gmail.com>
2015-11-24 15:40:15 +0000

    Added extra help text

--- models/box.py
+++ models/box.py
@@ -2,7 +2,8 @@ import datetime
 
 db.define_table('box',
                 Field('owner', 'reference auth_user', required=True, notnull=True, writable=False),
-                Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY()),
+                Field('name', required=True, notnull=True, requires=IS_NOT_EMPTY(),
+                      comment='Box names must be unique to your account.'),
                 Field('private', 'boolean', required=True, notnull=True,
                       comment='Private boxes can only be seen by you.'),
                 Field('created', 'datetime', required=True, notnull=True, default=datetime.datetime.now, writable=False)
